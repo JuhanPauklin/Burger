313ee84bb602d9baf51239aec584fee67881212a
Enriko <enrikokroon12@gmail.com>
2022-11-15 19:40:52 +0200

    "parandatud kujundus, lisatud tühja lahtri kontroll"

--- burger_generator/generator.js
+++ burger_generator/generator.js
@@ -4,14 +4,21 @@ const lisandid = [' juustuga', ' sinihallitusjuustuga',' sibulamoosiga', ' ketš
 const suurus = ['topelt ','kolmekordne ','maxi ','mega ','klassikaline '];
 
 function generate(){
+    //võtab teksitvälja sisu muutujasse
     let userInput = document.getElementById("userInput").value;
-    let suurusNumber = Math.floor(Math.random()* suurus.length)
-    let lisandNumber = Math.floor(Math.random()* lisandid.length)
+    // kui muutuja on tühi string siis annab hoiatuse, et tekstiväli on tühi
+    if(userInput === ""){
+        alert("Sisesta lahtrisse mingi väärtus ka!")
+    }else{
+        //genereeritakse suvalised lisand ja suurus ning lisatakse
+        //üheks stringiks kokku + sisestatakse <p> elementi id="output"
+        //viimasene muudetakse lahtri nähtavus nähtavaks    
+        let suurusNumber = Math.floor(Math.random()* suurus.length)
+        let lisandNumber = Math.floor(Math.random()* lisandid.length)
+        
+        outputField = document.getElementById("output");
+        outputField.innerHTML = suurus[suurusNumber]+String(userInput)+"burger"+lisandid[lisandNumber];
+        outputField.style.visibility = "visible";
 
-    outputField = document.getElementById("output");
-    outputField.innerHTML = suurus[suurusNumber]+String(userInput)+"burger"+lisandid[lisandNumber];
-    outputField.style.visibility = "visible";
-
-
-    return 
+    }
 }
